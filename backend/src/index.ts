90cf48d701928bc989e5aa99964e429f9dc3952f
catonnet <julien.catonnet@essec.edu>
2024-06-22 14:13:22 +0100

    setup database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
 
 const app = express();
 app.use(express.json());
@@ -13,5 +18,5 @@ app.get("/test", async (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log("Server started on localhost:8800");
+  console.log("Server started on https://localhost:8800");
 });
